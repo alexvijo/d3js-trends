c43cf1c1684d8dfede5ce909c479c527b0ee256e
ALEX\alex <alejandro.vicente@slashmobility.com>
2018-01-17 17:31:25 +0100

    webpack configfile

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+var path = require('path');
+module.exports = {
+    context: path.join(__dirname, 'src'),
+    entry: {
+        app: './app.js',
+        about: './about/about.js'
+    },    
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].bundle.js'
+    }
+};
